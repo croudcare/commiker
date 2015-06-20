92e559cb500e269eb8a812b4d931cf9c9a5c94cf
joaquimadraz <joaquim.adraz@gmail.com>
2015-06-20 22:26:50 +0100

    sprints index with pagination

--- public/js/angular/controllers/sprints/index_controller.js
+++ public/js/angular/controllers/sprints/index_controller.js
@@ -11,42 +11,37 @@
 
   function SprintsIndexController($state, $location, SprintSmooth) {
     var self = this;
+    self.page = 1;
+    self.perPage = 5;
 
-    self.sprints = [];
+    self.latestSprint = null;
+    self.previousSprints = [];
+    self.moreToLoad = true;
 
-    findSprint('active');
+    self.loadMoreSprints = loadMoreSprints;
 
-    function findSprint(id, cbSuccess) {
-      if(cbSuccess == undefined)
-        cbSuccess = onSuccess;
+    loadMoreSprints();
 
-      SprintSmooth.find('active')
-        .success(cbSuccess)
-        .error(onError)
-
-      function onSuccess(sprint, code) {
-        // no active sprint
-        if(code != 204)
-          $state.go('sprints-show', { id: 'active' });
-        else
-          listSprints();
+    function loadMoreSprints() {
+      if(self.moreToLoad) {
+        SprintSmooth.find('', { page: self.page, per_page: self.perPage })
+          .success(onSuccess);
       }
-
-      function onError(response) {
-        // debugger
-      }
-    }
-
-    function listSprints() {
-
-      SprintSmooth.find('')
-        .success(onSuccess);
 
       function onSuccess(sprints, code) {
-        self.latestSprint = sprints.shift();
-        self.previousSprints = sprints;
-      }
+        var sprintsCount = sprints.length;
 
+        if(self.page == 1)
+          self.latestSprint = sprints.shift();
+
+        self.previousSprints = self.previousSprints.concat(sprints);
+
+        // hide load more button if no more
+        if(sprintsCount < self.perPage)
+          self.moreToLoad = false;
+
+        self.page++;
+      }
     }
   }
 
