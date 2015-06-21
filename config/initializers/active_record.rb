22bad6183627d0bc5dc568cf8553427fbca60941
joaquimadraz <joaquim.adraz@gmail.com>
2015-06-21 16:40:40 +0100

    ar

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -1,6 +1,2 @@
 # establish db connection
-if ENV['RACK_ENV'] == 'production'
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-else
-  ActiveRecord::Base.establish_connection(YAML::load_file('./config/database.yml')[ENV["RACK_ENV"]])
-end
+ActiveRecord::Base.establish_connection(YAML::load_file('./config/database.yml')[ENV["RACK_ENV"]])
