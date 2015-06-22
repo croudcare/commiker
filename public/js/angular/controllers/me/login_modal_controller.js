e16c79971bbdb8cc62a5ab06312fc5e0528f3fad
joaquimadraz <joaquim.adraz@gmail.com>
2015-06-22 09:56:10 +0100

    tmp

--- public/js/angular/controllers/me/login_modal_controller.js
+++ public/js/angular/controllers/me/login_modal_controller.js
@@ -2,23 +2,13 @@
 
   commikerApp.controller('MeLoginModalController', [
     '$modalInstance',
-    'back',
     MeLoginModalController
   ]);
 
   /****************** PROTECTED ******************/
 
-  function MeLoginModalController($modalInstance, back) {
+  function MeLoginModalController($modalInstance) {
     var self = this;
-
-    self.backUrl = [];
-    self.backUrlStr = '';
-
-    if(back != undefined)
-      self.backUrl.push('origin=' + back);
-
-    if(self.backUrl.length > 0)
-       self.backUrlStr += '?' + self.backUrl.join('&');
 
     self.cancel = function () {
       $modalInstance.dismiss('cancel');
