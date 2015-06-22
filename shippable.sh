91a2e2319ce439ebfe97c3cf8e465f5a35cafb98
joaquimadraz <joaquim.adraz@gmail.com>
2015-06-22 18:09:36 +0100

    add shippable

---
+++ shippable.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+rm -f config/database.yml
+cat << EOF > config/database.yml
+test:
+  adapter: postgresql
+  encoding: unicode
+  database: commiker_test
+  host: localhost
+  pool: 25
+  username: postgres
+  password: postgres
+EOF
+
+rm -f config/config.yml
+cat << EOF > config/config.yml
+test:
+  AUTHENTICATION_SECRET: 'auth_test_secret'
+  SESSION_SECRET: 'session_test_secret'
+  PIVOTAL_TOKEN: 'pivotal_test_token'
+EOF
+
